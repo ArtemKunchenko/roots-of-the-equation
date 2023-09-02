e04c2066d64c330dbd696f0ecf31b28ed2f53fd9
Артем Кунченко <kunchenko911@ukr.net>
2023-09-02 18:45:23 +0300

    added class QuadraticEquation

--- roots of the equation/Source.cpp
+++ roots of the equation/Source.cpp
@@ -7,10 +7,10 @@ void Roots(RootsEquation& equastion) { equastion.Roots(); }
 
 int main()
 {
-	/*LinearEquation ex1;
-	Roots(ex1);*/
-	QuadraticEquation ex2;
-	Roots(ex2);
+	LinearEquation ex1;
+	Roots(ex1);
+	/*QuadraticEquation ex2;
+	Roots(ex2);*/
 
 	system("pause");
 	return 0;
